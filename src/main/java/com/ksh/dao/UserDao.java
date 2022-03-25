f5831a8246aa223e3ce4caa0c9a81d9cd4f6010f
KwakSeongHun <lkysh3@gmail.com>
2022-03-25 14:36:14 +0900

    맥 - 3장 종료

--- src/main/java/com/ksh/dao/UserDao.java
+++ src/main/java/com/ksh/dao/UserDao.java
@@ -1,180 +1,51 @@
 package com.ksh.dao;
 
 import com.ksh.domain.User;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
+    private final RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+            User user = new User();
+            user.setId(resultSet.getString("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        }
+    };
 
     public UserDao() {
     }
 
-//    public void setJdbcContext(JdbcContext jdbcContext){
-//        this.jdbcContext = jdbcContext;
-//    }
-
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-
-        // set 메소드이면서 jdbcContext에 대한 생성과 DI를 수행한다.
-        // UserDao가 JdbcContext 대신 DI를 받아서 JdbcContext에게 넘겨주는 역할만 한다.
-        // 인터페이스를 사용해서 DI를 하는것이 정석이지만 둘이 항상 함께 사용되어야 하는 경우라면 클래스로 DI를 해도 괜찮다.
-        // 이때는 jdbcContext를 싱글톤으로 만들수 없다.
-        this.jdbcContext = new JdbcContext();
-        this.jdbcContext.setDataSource(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) throws SQLException {
-        // 메소드 내부에 선언한 내부 클래스. 이 메소드 안에서만 사용할 수 있다.
-        // 내부 클래스는 선언된 메소드의 로컬 변수에 직접 접근이 가능하다. 메소드 파라미터도 로컬 변수다.
-        // 그때 외부변수는 final로 선언되어 있어야 한다.
-//        class AddStatement implements StatementStrategy{
-//            @Override
-//            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-//                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-//                ps.setString(1, user.getId());
-//                ps.setString(2, user.getName());
-//                ps.setString(3, user.getPassword());
-//
-//                return ps;
-//            }
-//        }
-
-        // 익명 내부 클래스 방식
-        // 생성과 동시에 인스턴스를 생성한다.
-        // 이름이 없기 때문에 자신의 타입은 가질수 없고 인터페이스 타입의 변수에만 저장 가능하다.
-//        StatementStrategy st = new StatementStrategy() {
-//            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-//                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-//                ps.setString(1, user.getId());
-//                ps.setString(2, user.getName());
-//                ps.setString(3, user.getPassword());
-//
-//                return ps;
-//            }
-//        };
-//        jdbcContextWithStatementStrategy(st);
-
-        // 위의 코드를 간결하게 정리한 것.
-        // 메소드 내에서 한번만 쓰고 재사용할일이 없다면 이렇게 정리해도 된다.
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-
-                return ps;
-            }
-        });
+    public void add(final User user){
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("ID"));
-            user.setName(rs.getString("NAME"));
-            user.setPassword(rs.getString("PASSWORD"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if (user == null)
-            throw new EmptyResultDataAccessException(1);
-
-        return user;
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, userMapper);
     }
 
-    // 클라이언트 : deleteAll
-    // 템플릿 : jdbcContext.workWithStatementStrategy
-    // 콜백 : new StatementStrategy
-    // 전략 패턴에서는 클라이언트가 전략 오브젝트(new StatementStrategy)를 만들고 컨텍스트(jdbcContext.workWithStatementStrategy)를 호출할때 넘겨준다.
-    public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+    public List<User> getAll(){
+        return this.jdbcTemplate.query("select * from users order by id", userMapper);
     }
 
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
 
-
-    // 리턴값이 있는 경우에는 어떻게 할지 생각
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
-
-    // 컨텍스트 코드
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            ps = stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        // queryForInt는 Deprecated 되었음.
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
--- src/test/java/com/ksh/dao/UserDaoTest.java
+++ src/test/java/com/ksh/dao/UserDaoTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -46,12 +47,11 @@ public class UserDaoTest {
 
 //        this.dao = this.context.getBean("userDao", UserDao.class);
 
+        // 픽스쳐
         user1 = new User("lkysh3", "곽성훈", "eksxp123");
         user2 = new User("leegw70", "이길원", "springno2");
         user3 = new User("bumjin", "박범진", "springno3");
 
-//        System.out.println(this.context);
-//        System.out.println(this);
     }
 
     @Test
@@ -96,5 +96,38 @@ public class UserDaoTest {
         assertThat(dao.getCount(), is(0));
 
         dao.get("unknown_id");
+    }
+
+    @Test
+    public void getAll(){
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(1));
+        checkSameUser(user2, users2.get(0));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user1, users3.get(2));
+        checkSameUser(user2, users3.get(1));
+        checkSameUser(user3, users3.get(0));
+    }
+
+    // User 클래스의 변수값들이 서로 같은지 비교
+    public void checkSameUser(User user1, User user2){
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 }
