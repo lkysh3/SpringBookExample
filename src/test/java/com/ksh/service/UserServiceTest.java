544e3f515d63429650fae67f09ef2a6d1cf075f5
KwakSeongHun <lkysh3@gmail.com>
2022-08-17 22:35:50 +0900

    맥 - 6.2장 종료

--- src/test/java/com/ksh/service/UserServiceTest.java
+++ src/test/java/com/ksh/service/UserServiceTest.java
@@ -3,9 +3,12 @@ package com.ksh.service;
 import com.ksh.dao.UserDao;
 import com.ksh.domain.Grade;
 import com.ksh.domain.User;
+import com.sun.mail.iap.Argument;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
@@ -15,6 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.PlatformTransactionManager;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -26,6 +30,7 @@ import static com.ksh.service.UserServiceImpl.MIN_RECOMMEND_FOR_GOLD;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -68,29 +73,77 @@ public class UserServiceTest {
     }
 
     @Test
+    public void mockUpgradeGrades() throws Exception{
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+        UserDao mockUserDao = mock(UserDao.class);
+        when(mockUserDao.getAll()).thenReturn(this.users);
+        userServiceImpl.setUserDao(mockUserDao);
+
+        MailSender mockMailSender = mock(MailSender.class);
+        userServiceImpl.setMailSender(mockMailSender);
+
+        userServiceImpl.upgradeGrades();
+
+        verify(mockUserDao, times(2)).update(Matchers.any(User.class));
+        verify(mockUserDao, times(2)).update(Matchers.any(User.class));
+        verify(mockUserDao).update(users.get(1));
+        assertThat(users.get(1).getGrade(), is(Grade.SILVER));
+        verify(mockUserDao).update(users.get(3));
+        assertThat(users.get(3).getGrade(), is(Grade.GOLD));
+
+        ArgumentCaptor<SimpleMailMessage> mailMessageArg = ArgumentCaptor.forClass(SimpleMailMessage.class);
+        verify(mockMailSender, times(2)).send(mailMessageArg.capture());
+        List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues();
+        assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail()));
+        assertThat(mailMessages.get(1).getTo()[0], is(users.get(3).getEmail()));
+    }
+
+    @Test
     @DirtiesContext
     public void upgradeGrades() throws Exception{
-        userDao.deleteAll();
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
 
-        for (User user : users) {
-            userDao.add(user);
-        }
+        MockUserDao mockUserDao = new MockUserDao(this.users);
+        userServiceImpl.setUserDao(mockUserDao);
 
         MockMailSender mockMailSender = new MockMailSender();
         userServiceImpl.setMailSender(mockMailSender);
 
-        userService.upgradeGrades();
+        userServiceImpl.upgradeGrades();
 
-        checkGradeUpgraded(users.get(0), false);
-        checkGradeUpgraded(users.get(1), true);
-        checkGradeUpgraded(users.get(2), false);
-        checkGradeUpgraded(users.get(3), true);
-        checkGradeUpgraded(users.get(4), false);
+        List<User> updated = mockUserDao.getUpdated();
+        assertThat(updated.size(), is(2));
+        checkUserAndGrade(updated.get(0), "joytouch", Grade.SILVER);
+        checkUserAndGrade(updated.get(1), "madnite1", Grade.GOLD);
 
         List<String> request = mockMailSender.getRequests();
         assertThat(request.size(), is(2));
         assertThat(request.get(0), is(users.get(1).getEmail()));
         assertThat(request.get(1), is(users.get(3).getEmail()));
+
+
+//        userDao.deleteAll();
+//
+//        for (User user : users) {
+//            userDao.add(user);
+//        }
+//
+//        MockMailSender mockMailSender = new MockMailSender();
+//        userServiceImpl.setMailSender(mockMailSender);
+//
+//        userService.upgradeGrades();
+//
+//        checkGradeUpgraded(users.get(0), false);
+//        checkGradeUpgraded(users.get(1), true);
+//        checkGradeUpgraded(users.get(2), false);
+//        checkGradeUpgraded(users.get(3), true);
+//        checkGradeUpgraded(users.get(4), false);
+//
+//        List<String> request = mockMailSender.getRequests();
+//        assertThat(request.size(), is(2));
+//        assertThat(request.get(0), is(users.get(1).getEmail()));
+//        assertThat(request.get(1), is(users.get(3).getEmail()));
     }
 
     private void checkGradeUpgraded(User user, boolean upgraded){
@@ -100,6 +153,11 @@ public class UserServiceTest {
         }else{
             assertThat(userUpdate.getGrade(), is(user.getGrade()));
         }
+    }
+
+    private void checkUserAndGrade(User updated, String expectedId, Grade expectedGrade){
+        assertThat(updated.getId(), is(expectedId));
+        assertThat(updated.getGrade(), is(expectedGrade));
     }
 
     @Test
@@ -182,5 +240,52 @@ public class UserServiceTest {
         public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
 
         }
+    }
+
+    static class MockUserDao implements UserDao{
+        private List<User> users;
+        private List<User> updated = new ArrayList();
+
+        private MockUserDao(List<User> users){
+            this.users = users;
+        }
+
+        // 등급 업그레이드 대상 사용자 확인용
+        public List<User> getUpdated(){
+            return this.updated;
+        }
+
+        // 스텁 기능 제공
+        @Override
+        public List<User> getAll() {
+            return this.users;
+        }
+
+        @Override
+        public void update(User user) {
+            updated.add(user);
+        }
+
+        @Override
+        public void add(User user) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public User get(String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void deleteAll() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int getCount() {
+            throw new UnsupportedOperationException();
+        }
+
+
     }
 }
